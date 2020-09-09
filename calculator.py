7fc80ab98382e28ec666b765a161621ca4b77e66
AlesandroAndreev <alexander_597@mail.ru>
2020-09-10 01:16:47 +0300

    I create calculatorgit add calculator.py!

---
+++ calculator.py
@@ -0,0 +1,44 @@
+for i in range(1000):
+    print("Hello! This is calculator. To exit write exit")
+#Operator input
+    b = input("Write operator: ")
+#Check exit condition
+    if b == "exit":
+        print("Bye!")
+        break
+#Entering numbers
+    a = int(input("Write first number: "))
+    c = int(input("Write second number: "))
+
+#Basic operations of the calculator.
+    # Before division, it is checked that
+    # the denominator is not zero.
+
+    if b == "+" :
+        print(a + c)
+    elif b == "-":
+        print(a - c)
+    elif b == "*":
+        print(a * c)
+    elif b == "/":
+        if c == 0:
+            print("Zero!")
+            continue
+        print(a / c)
+    elif b == "**":
+        print(a ** c)
+    elif b == "%":
+        if c == 0:
+            print("Zero!")
+            continue
+        print(a % c)
+    elif b == "//":
+        if c == 0:
+            print("Zero!")
+            continue
+        print(a // c)
+    else:
+        print("Error, wrong operand")
+        continue
+
+
